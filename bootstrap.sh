a70f03d6d72b82506b263da57e0fe9449914753b
clinton <clinton.freeman@gmail.com>
2015-09-18 13:55:41 +1000

    Added bash script for bootstrapping OpenCV.

---
+++ bootstrap.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+# Update aptitude
+sudo apt-get update
+sudo apt-get upgrade -y
+
+# Install opencv & vim.
+sudo apt-get install -y libopencv-dev
+sudo apt-get install -y vim
+
+if [ ! -f go1.4.2.linux-arm~multiarch-armv7-1.tar.gz ]; then
+	wget http://dave.cheney.net/paste/go1.4.2.linux-arm~multiarch-armv7-1.tar.gz
+fi
+
+sudo tar -C /usr/local -xzf go1.4.2.linux-arm~multiarch-armv7-1.tar.gz
+echo "export PATH=$PATH:/usr/local/go/bin" >> /home/pi/.bashrc
+source /home/pi/.bashrc
