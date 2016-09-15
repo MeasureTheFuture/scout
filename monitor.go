eb3ccf8e09f8e5314b907e2c875c4c8ed53f4e8d
clinton <clinton.freeman@gmail.com>
2016-09-15 15:00:40 +1000

    #10 - patched memory leak in OpenCV calls.

--- monitor.go
+++ monitor.go
@@ -236,6 +236,7 @@ func measure(deltaC chan Command, videoFile string, debug bool, config Configura
 		}
 
 		scene.update(detectedObjects, debug, config)
+		C.cvReleaseMemStorage(&storage)
 
 		if debug {
 			var font C.CvFont
